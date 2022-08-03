c60b36eac6a6c6684e58511717941773f2fd7be1
zxh0 <zhangxiuhong2013@gmail.com>
2022-08-03 18:04:42 +0800

    Split CCMonitorsGov and CCOperatorsGov

---
+++ contracts/CCMonitorsGov.sol
@@ -0,0 +1,97 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract CCMonitorsGov {
+
+    struct MonitorInfo {
+        address addr;         // address
+        uint    pubkeyPrefix; // 0x02 or 0x03 (TODO: change type to uint8)
+        bytes32 pubkeyX;      // x
+        bytes32 intro;        // introduction
+        uint    stakedAmt;    // in BCH
+        uint    electedTime;  // 0 means not elected, set by Golang
+    }
+
+    event MonitorApply(address indexed candidate, uint pubkeyPrefix, bytes32 pubkeyX, bytes32 intro, uint initStakeAmt);
+    event MonitorStake(address indexed candidate, uint amt);
+    event MonitorUnstake(address indexed candidate, uint amt);
+
+    uint constant MIN_STAKE = 100_000 ether;
+    uint constant UNSTAKE_WINDOW = 10 days;
+
+    uint public lastElectionTime;  // set by Golang
+    MonitorInfo[] monitors; // read by Golang
+    mapping(address => uint) monitorIdxByAddr;
+    uint[] freeSlots;
+
+    function applyMonitor(uint8 pubkeyPrefix,
+                          bytes32 pubkeyX,
+                          bytes32 intro) public payable {
+        require(pubkeyPrefix == 0x02 || pubkeyPrefix == 0x03, 'invalid-pubkey-prefix');
+        require(msg.value >= MIN_STAKE, 'deposit-too-less');
+        require(monitorIdxByAddr[msg.sender] == 0, 'monitor-existed');
+
+        if (freeSlots.length >= 0) {
+            uint freeSlot = freeSlots[freeSlots.length - 1];
+            freeSlots.pop();
+            monitors[freeSlot] = MonitorInfo(msg.sender, pubkeyPrefix, pubkeyX, intro, msg.value, 0);
+            monitorIdxByAddr[msg.sender] = freeSlot;
+        } else {
+            monitors.push(MonitorInfo(msg.sender, pubkeyPrefix, pubkeyX, intro, msg.value, 0));
+            monitorIdxByAddr[msg.sender] = monitors.length - 1;
+        }
+
+        emit MonitorApply(msg.sender, pubkeyPrefix, pubkeyX, intro, msg.value);
+    }
+
+    function addStake() public payable {
+        uint monitorIdx = monitorIdxByAddr[msg.sender];
+        MonitorInfo storage monitor = monitors[monitorIdx];
+        require(monitor.addr == msg.sender, 'no-such-monitor');
+        require(msg.value > 0, 'deposit-nothing');
+        monitor.stakedAmt += msg.value;
+        emit MonitorStake(msg.sender, msg.value);
+    }
+
+    function removeStake(uint amt) public {
+        uint monitorIdx = monitorIdxByAddr[msg.sender];
+        MonitorInfo storage monitor = monitors[monitorIdx];
+        require(monitor.addr == msg.sender, 'no-such-monitor');
+        require(monitor.stakedAmt >= amt, 'withdraw-too-much');
+
+        monitor.stakedAmt += amt;
+        if (monitor.stakedAmt < MIN_STAKE) {
+            require(monitor.electedTime == 0, 'monitor-is-active');
+            require(block.timestamp - lastElectionTime < UNSTAKE_WINDOW, "outside-unstake-window");
+            // TODO: more checks
+        }
+        if (monitor.stakedAmt == 0) {
+            delete monitors[monitorIdx];
+            delete monitorIdxByAddr[msg.sender];
+            freeSlots.push(monitorIdx);
+        }
+
+        Address.sendValue(payable(msg.sender), amt);
+        emit MonitorUnstake(msg.sender, amt);
+    }
+
+}
+
+
+contract CCMonitorsGovForTest is CCMonitorsGov {
+
+    function setLastElectionTime(uint ts) public {
+        lastElectionTime = ts;
+    }
+
+    function addMonitor(uint pubkeyPrefix,
+                        bytes32 pubkeyX,
+                        bytes32 intro,
+                        uint stakedAmt) public {
+        monitors.push(MonitorInfo(msg.sender, 
+            pubkeyPrefix, pubkeyX, intro, stakedAmt, 0));
+    }
+
+}
---
+++ contracts/CCOperatorsGov.sol
@@ -0,0 +1,124 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract CCOperatorsGov {
+
+    struct OperatorInfo {
+        address addr;           // address
+        uint    pubkeyPrefix;   // 0x02 or 0x03 (TODO: change type to uint8)
+        bytes32 pubkeyX;        // x
+        bytes32 rpcUrl;         // ip:port
+        bytes32 intro;          // introduction
+        uint    totalStakedAmt; // in BCH
+        uint    selfStakedAmt;  // in BCH
+        uint    electedTime;    // 0 means not elected, set by Golang
+    }
+
+    struct StakeInfo {
+        address staker;
+        address operator;
+        uint32  stakedTime;
+        uint    stakedAmt;
+    }
+
+    event OperatorApply(address indexed candidate, uint pubkeyPrefix, bytes32 pubkeyX, bytes32 rpcUrl, bytes32 intro, uint initStakeAmt);
+    event OperatorStake(address indexed candidate, address indexed staker, uint stakeId, uint amt);
+    event OperatorUnstake(address indexed candidate, address indexed staker, uint stakeId, uint amt);
+
+    uint constant INIT_STAKE = 10_000 ether;
+    uint constant MIN_STAKE_PERIOD = 100 days;
+
+    OperatorInfo[] operators; // read by Golang
+    mapping(address => uint) operatorIdxByAddr;
+    uint[] freeSlots;
+
+    uint lastStakeId;
+    mapping(uint => StakeInfo) stakeById;
+
+
+    function applyOperator(uint8 pubkeyPrefix,
+                           bytes32 pubkeyX,
+                           bytes32 rpcUrl, 
+                           bytes32 intro) public payable {
+        require(pubkeyPrefix == 0x02 || pubkeyPrefix == 0x03, 'invalid-pubkey-prefix');
+        require(msg.value >= INIT_STAKE, 'deposit-too-less');
+        require(operatorIdxByAddr[msg.sender] == 0, 'operator-existed');
+
+        if (freeSlots.length >= 0) {
+            uint freeSlot = freeSlots[freeSlots.length - 1];
+            freeSlots.pop();
+            operators[freeSlot] = OperatorInfo(msg.sender, pubkeyPrefix, pubkeyX, rpcUrl, intro, msg.value, msg.value, 0);
+            operatorIdxByAddr[msg.sender] = freeSlot;
+        } else {
+            operators.push(OperatorInfo(msg.sender, pubkeyPrefix, pubkeyX, rpcUrl, intro, msg.value, msg.value, 0));
+            operatorIdxByAddr[msg.sender] = operators.length - 1;
+        }
+
+        stakeById[++lastStakeId] = StakeInfo(msg.sender, msg.sender, uint32(block.timestamp), msg.value);
+        emit OperatorApply(msg.sender, pubkeyPrefix, pubkeyX, rpcUrl, intro, msg.value);
+    }
+
+    function stakeOperator(address addr) public payable {
+        require(msg.value > 0, 'deposit-nothing');
+        uint stakeId = ++lastStakeId;
+        stakeById[stakeId] = StakeInfo(msg.sender, addr, uint32(block.timestamp), msg.value);
+
+        uint operatorIdx = operatorIdxByAddr[addr];
+        OperatorInfo storage operator = operators[operatorIdx];
+        require(operator.addr == addr, 'no-such-operator');
+        operator.totalStakedAmt += msg.value;
+        if (addr == msg.sender) {
+            operator.selfStakedAmt += msg.value;
+        }
+        emit OperatorStake(operator.addr, msg.sender, stakeId, msg.value);
+    }
+
+    function unstakeOperator(uint stakeId, uint amt) public {
+        StakeInfo storage stakeInfo = stakeById[stakeId];
+        require(stakeInfo.stakedAmt >= amt, 'withdraw-too-much');
+        require(stakeInfo.stakedTime + MIN_STAKE_PERIOD < block.timestamp, 'not-mature');
+
+        uint operatorIdx = operatorIdxByAddr[stakeInfo.operator];
+        OperatorInfo storage operator = operators[operatorIdx];
+        require(operator.addr != address(0), 'no-such-operator');
+
+        operator.totalStakedAmt -= amt;
+        if (operator.addr == msg.sender) {
+            operator.selfStakedAmt -= amt;
+            if (operator.electedTime > 0) {
+                require(operator.selfStakedAmt > INIT_STAKE, 'too-less-self-stake');
+            }
+        }
+        if (operator.totalStakedAmt == 0) {
+            delete operators[operatorIdx];
+            delete operatorIdxByAddr[msg.sender];
+            freeSlots.push(operatorIdx);
+        }
+
+        stakeInfo.stakedAmt -= amt;
+        if (stakeInfo.stakedAmt == 0) {
+            delete stakeById[stakeId];
+        }
+
+        Address.sendValue(payable(msg.sender), amt);
+        emit OperatorUnstake(operator.addr, msg.sender, stakeId, amt);
+    }
+
+}
+
+
+contract CCOperatorsGovForTest is CCOperatorsGov {
+
+    function addOperator(uint pubkeyPrefix,
+                         bytes32 pubkeyX,
+                         bytes32 rpcUrl, 
+                         bytes32 intro,
+                         uint totalStakedAmt,
+                         uint selfStakedAmt) public {
+        operators.push(OperatorInfo(msg.sender, 
+            pubkeyPrefix, pubkeyX, rpcUrl, intro, totalStakedAmt, selfStakedAmt, 0));
+    }
+
+}
