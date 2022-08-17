9ac84bf3f9bf40a4a25f6f5873c32bde4a54f1cc
zxh0 <zhangxiuhong2013@gmail.com>
2022-08-17 12:19:02 +0800

    Add more tests for CCOperatorsGov

--- test/CCOperatorsGov.js
+++ test/CCOperatorsGov.js
@@ -5,16 +5,25 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-const testPubKeyX1 = ethers.utils.formatBytes32String('pubkey1');
-const testPubKeyX2 = ethers.utils.formatBytes32String('pubkey2');
-const testPubKeyX3 = ethers.utils.formatBytes32String('pubkey3');
+const zeroAddr = '0x0000000000000000000000000000000000000000';
+const zeroBytes32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+const testPkX1     = ethers.utils.formatBytes32String('pubkey1');
+const testPkX2     = ethers.utils.formatBytes32String('pubkey2');
+const testPkX3     = ethers.utils.formatBytes32String('pubkey3');
+const testPkX4     = ethers.utils.formatBytes32String('pubkey4');
+const testPkX5     = ethers.utils.formatBytes32String('pubkey5');
 const testRpcUrl1  = ethers.utils.formatBytes32String('rpcUrl1');
 const testRpcUrl2  = ethers.utils.formatBytes32String('rpcUrl2');
 const testRpcUrl3  = ethers.utils.formatBytes32String('rpcUrl3');
+const testRpcUrl4  = ethers.utils.formatBytes32String('rpcUrl4');
+const testRpcUrl5  = ethers.utils.formatBytes32String('rpcUrl5');
 const testIntro1   = ethers.utils.formatBytes32String('intro11');
 const testIntro2   = ethers.utils.formatBytes32String('intro22');
 const testIntro3   = ethers.utils.formatBytes32String('intro33');
-const [testPubKeyX, testRpcUrl, testIntro] = [testPubKeyX1, testRpcUrl1, testIntro1];
+const testIntro4   = ethers.utils.formatBytes32String('intro44');
+const testIntro5   = ethers.utils.formatBytes32String('intro55');
+const [testPkX, testRpcUrl, testIntro] = [testPkX1, testRpcUrl1, testIntro1];
 
 
 const minSelfStakeAmt = ethers.utils.parseUnits('10000');
@@ -23,19 +32,19 @@ const MIN_STAKE_PERIOD = 100 * 24 * 3600;
 describe("CCOperatorsGov", function () {
 
   async function deployGov() {
-    const [op1, op2, op3] = await ethers.getSigners();
+    const [op1, op2, op3, op4, op5] = await ethers.getSigners();
 
     const OpsGov = await ethers.getContractFactory("CCOperatorsGovForUT");
     const gov = await OpsGov.deploy();
 
-    return { gov, op1, op2, op3 };
+    return { gov, op1, op2, op3, op4, op5 };
   }
 
   it("applyOperator: invalid-pubkey-prefix", async () => {
     const { gov } = await loadFixture(deployGov);
     const testCases = [0x00, 0x01, 0x04, 0x05, 0x09, 0x99, 0xff];
     for (const x of testCases) {
-      await expect(gov.applyOperator(x, testPubKeyX, testRpcUrl, testIntro))
+      await expect(gov.applyOperator(x, testPkX, testRpcUrl, testIntro))
         .to.be.revertedWith("invalid-pubkey-prefix");
     }
   });
@@ -44,7 +53,7 @@ describe("CCOperatorsGov", function () {
     const { gov } = await loadFixture(deployGov);
     const testCases = [0, 1, 123, minSelfStakeAmt.sub(1)];
     for (const x of testCases) {
-      await expect(gov.applyOperator(0x02, testPubKeyX, testRpcUrl, testIntro, {value: x}))
+      await expect(gov.applyOperator(0x02, testPkX, testRpcUrl, testIntro, {value: x}))
           .to.be.revertedWith("deposit-too-less");
     }
   });
@@ -52,8 +61,8 @@ describe("CCOperatorsGov", function () {
   it("applyOperator: operator-existed", async () => {
     const { gov, op1, op2, op3 } = await loadFixture(deployGov);
     for (const op of [op1, op2, op3]) {
-      await gov.connect(op).applyOperator(0x02, testPubKeyX, testRpcUrl, testIntro, {value: minSelfStakeAmt});
-      await expect(gov.connect(op).applyOperator(0x02, testPubKeyX, testRpcUrl, testIntro, {value: minSelfStakeAmt.add(1)}))
+      await gov.connect(op).applyOperator(0x02, testPkX, testRpcUrl, testIntro, {value: minSelfStakeAmt});
+      await expect(gov.connect(op).applyOperator(0x02, testPkX, testRpcUrl, testIntro, {value: minSelfStakeAmt.add(1)}))
           .to.be.revertedWith("operator-existed");
     }
   });
@@ -61,37 +70,36 @@ describe("CCOperatorsGov", function () {
   it("applyOperator: ok", async () => {
     const { gov, op1, op2, op3 } = await loadFixture(deployGov);
 
-    await expect(gov.connect(op1).applyOperator(0x02, testPubKeyX1, testRpcUrl1, testIntro1, {value: minSelfStakeAmt.add(1)}))
-        .to.emit(gov, 'OperatorApply').withArgs(op1.address, 0x02, testPubKeyX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1))
+    await expect(gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1, {value: minSelfStakeAmt.add(1)}))
+        .to.emit(gov, 'OperatorApply').withArgs(op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1))
         .to.emit(gov, 'OperatorStake').withArgs(op1.address, op1.address, 0, minSelfStakeAmt.add(1));;
 
-    await expect(gov.connect(op2).applyOperator(0x03, testPubKeyX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(2)}))
-        .to.emit(gov, 'OperatorApply').withArgs(op2.address, 0x03, testPubKeyX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2))
+    await expect(gov.connect(op2).applyOperator(0x03, testPkX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(2)}))
+        .to.emit(gov, 'OperatorApply').withArgs(op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2))
         .to.emit(gov, 'OperatorStake').withArgs(op2.address, op2.address, 1, minSelfStakeAmt.add(2));
 
-    await expect(gov.connect(op3).applyOperator(0x02, testPubKeyX3, testRpcUrl3, testIntro3, {value: minSelfStakeAmt.add(3)}))
-        .to.emit(gov, 'OperatorApply').withArgs(op3.address, 0x02, testPubKeyX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3))
+    await expect(gov.connect(op3).applyOperator(0x02, testPkX3, testRpcUrl3, testIntro3, {value: minSelfStakeAmt.add(3)}))
+        .to.emit(gov, 'OperatorApply').withArgs(op3.address, 0x02, testPkX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3))
         .to.emit(gov, 'OperatorStake').withArgs(op3.address, op3.address, 2, minSelfStakeAmt.add(3));
 
-    const ops = await getAllOperatorInfos(gov);
-    expect(ops.map(op => op.addr))          .to.deep.equal([op1.address, op2.address, op3.address]);
-    expect(ops.map(op => op.pubkeyPrefix))  .to.deep.equal([0x02, 0x03, 0x02]);
-    expect(ops.map(op => op.pubkeyX))       .to.deep.equal([testPubKeyX1, testPubKeyX2, testPubKeyX3]);
-    expect(ops.map(op => op.rpcUrl))        .to.deep.equal([testRpcUrl1, testRpcUrl2, testRpcUrl3]);
-    expect(ops.map(op => op.totalStakedAmt)).to.deep.equal([minSelfStakeAmt.add(1), minSelfStakeAmt.add(2), minSelfStakeAmt.add(3)]);
-    expect(ops.map(op => op.selfStakedAmt)) .to.deep.equal([minSelfStakeAmt.add(1), minSelfStakeAmt.add(2), minSelfStakeAmt.add(3)]);
-    expect(ops.map(op => op.electedTime))   .to.deep.equal([0, 0, 0]);
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,        totalStakedAmt,    electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(1), 0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2), minSelfStakeAmt.add(2), 0],
+      [op3.address, 0x02, testPkX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3), minSelfStakeAmt.add(3), 0],
+    ]);
 
-    const stakeInfos = await getAllStakeInfos(gov);
-    expect(stakeInfos.map(x => x.staker))   .to.deep.equal([op1.address, op2.address, op3.address]);
-    expect(stakeInfos.map(x => x.operator)) .to.deep.equal([op1.address, op2.address, op3.address]);
-    expect(stakeInfos.map(x => x.stakedAmt)).to.deep.equal([minSelfStakeAmt.add(1), minSelfStakeAmt.add(2), minSelfStakeAmt.add(3)]);
-    expect(stakeInfos.map(x => x.stakedTime > 0)).to.deep.equal([true, true, true]);
+    expect(await getAllStakeInfos(gov)).to.deep.equal([
+      // staker,     operator,       stakedAmt,         stakedTimeGT0
+      [op1.address, op1.address, minSelfStakeAmt.add(1), true],
+      [op2.address, op2.address, minSelfStakeAmt.add(2), true],
+      [op3.address, op3.address, minSelfStakeAmt.add(3), true],
+    ]);
   });
 
   it("stake: errors", async () => {
     const { gov, op1, op2, op3 } = await loadFixture(deployGov);
-    await gov.connect(op1).applyOperator(0x02, testPubKeyX1, testRpcUrl1, testIntro1,
+    await gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1,
       {value: minSelfStakeAmt.add(1)});
 
     await expect(gov.connect(op3).stakeOperator(op1.address))
@@ -104,7 +112,7 @@ describe("CCOperatorsGov", function () {
 
   it("unstake: errors", async () => {
     const { gov, op1, op2, op3 } = await loadFixture(deployGov);
-    await gov.connect(op1).applyOperator(0x02, testPubKeyX1, testRpcUrl1, testIntro1,
+    await gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1,
       {value: minSelfStakeAmt.add(1)});                              // stake#0
     await gov.connect(op2).stakeOperator(op1.address, {value: 123}); // stake#1
     await gov.connect(op3).stakeOperator(op1.address, {value: 456}); // stake#2
@@ -124,8 +132,142 @@ describe("CCOperatorsGov", function () {
           .to.be.revertedWith("too-less-self-stake");
   });
 
-  it("stake/unstake: ok", async () => {
-    // TODO
+  it("stake: ok", async () => {
+    const { gov, op1, op2, op3, op4, op5 } = await loadFixture(deployGov);
+
+    // stake#0,1
+    await gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1, {value: minSelfStakeAmt.add(1)});
+    await gov.connect(op2).applyOperator(0x03, testPkX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(2)});
+
+    // stake#2,3,4
+    await expect(gov.connect(op3).stakeOperator(op1.address, {value: 100}))
+        .to.emit(gov, 'OperatorStake').withArgs(op1.address, op3.address, 2, 100);
+    await expect(gov.connect(op3).stakeOperator(op2.address, {value: 200}))
+        .to.emit(gov, 'OperatorStake').withArgs(op2.address, op3.address, 3, 200);
+    await expect(gov.connect(op4).stakeOperator(op1.address, {value: 300}))
+        .to.emit(gov, 'OperatorStake').withArgs(op1.address, op4.address, 4, 300);
+
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,        totalStakedAmt,    electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(401), 0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2), minSelfStakeAmt.add(202), 0],
+    ]);
+
+    expect(await getAllStakeInfos(gov)).to.deep.equal([
+      // staker,     operator,       stakedAmt,         stakedTimeGT0
+      [op1.address, op1.address, minSelfStakeAmt.add(1), true],
+      [op2.address, op2.address, minSelfStakeAmt.add(2), true],
+      [op3.address, op1.address,                    100, true],
+      [op3.address, op2.address,                    200, true],
+      [op4.address, op1.address,                    300, true],
+    ]);
+  });
+
+  it("unstake: ok", async () => {
+    const { gov, op1, op2, op3, op4, op5 } = await loadFixture(deployGov);
+
+    // stake#0,1
+    await gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1, {value: minSelfStakeAmt.add(1)});
+    await gov.connect(op2).applyOperator(0x03, testPkX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(2)});
+
+    // stake#2,3,4,5
+    await gov.connect(op2).stakeOperator(op2.address, {value: 200});
+    await gov.connect(op3).stakeOperator(op2.address, {value: 300});
+    await gov.connect(op4).stakeOperator(op2.address, {value: 400});
+    await gov.connect(op5).stakeOperator(op2.address, {value: 500});
+
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,          totalStakedAmt,    electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1),   minSelfStakeAmt.add(1),    0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(202), minSelfStakeAmt.add(1402), 0],
+    ]);
+    expect(await getAllStakeInfos(gov)).to.deep.equal([
+      // staker,     operator,       stakedAmt,         stakedTimeGT0
+      [op1.address, op1.address, minSelfStakeAmt.add(1), true],
+      [op2.address, op2.address, minSelfStakeAmt.add(2), true],
+      [op2.address, op2.address,                    200, true],
+      [op3.address, op2.address,                    300, true],
+      [op4.address, op2.address,                    400, true],
+      [op5.address, op2.address,                    500, true],
+    ]);
+
+    await time.increase(MIN_STAKE_PERIOD + 1);
+    await expect(gov.connect(op2).unstakeOperator(2, 100))
+        .to.emit(gov, 'OperatorUnstake').withArgs(op2.address, op2.address, 2, 100);
+    await expect(gov.connect(op4).unstakeOperator(4, 150))
+        .to.emit(gov, 'OperatorUnstake').withArgs(op2.address, op4.address, 4, 150);
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,          totalStakedAmt,    electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1),   minSelfStakeAmt.add(1),    0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(102), minSelfStakeAmt.add(1152), 0],
+    ]);
+    expect(await getAllStakeInfos(gov)).to.deep.equal([
+      // staker,     operator,       stakedAmt,         stakedTimeGT0
+      [op1.address, op1.address, minSelfStakeAmt.add(1), true],
+      [op2.address, op2.address, minSelfStakeAmt.add(2), true],
+      [op2.address, op2.address,                    100, true],
+      [op3.address, op2.address,                    300, true],
+      [op4.address, op2.address,                    250, true],
+      [op5.address, op2.address,                    500, true],
+    ]);
+
+    await expect(gov.connect(op2).unstakeOperator(2, 100))
+        .to.emit(gov, 'OperatorUnstake').withArgs(op2.address, op2.address, 2, 100);
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,        totalStakedAmt,    electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(1),    0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2), minSelfStakeAmt.add(1052), 0],
+    ]);
+    expect(await getAllStakeInfos(gov)).to.deep.equal([
+      // staker,     operator,       stakedAmt,         stakedTimeGT0
+      [op1.address, op1.address, minSelfStakeAmt.add(1), true],
+      [op2.address, op2.address, minSelfStakeAmt.add(2), true],
+      [zeroAddr,    zeroAddr,                         0,false],
+      [op3.address, op2.address,                    300, true],
+      [op4.address, op2.address,                    250, true],
+      [op5.address, op2.address,                    500, true],
+    ]);
+  });
+
+  it("stake/unstake: operator slots", async () => {
+    const { gov, op1, op2, op3, op4, op5 } = await loadFixture(deployGov);
+
+    await gov.connect(op1).applyOperator(0x02, testPkX1, testRpcUrl1, testIntro1, {value: minSelfStakeAmt.add(1)});
+    await gov.connect(op2).applyOperator(0x03, testPkX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(2)});
+    await gov.connect(op3).applyOperator(0x02, testPkX3, testRpcUrl3, testIntro3, {value: minSelfStakeAmt.add(3)});
+    await gov.connect(op4).applyOperator(0x03, testPkX4, testRpcUrl4, testIntro4, {value: minSelfStakeAmt.add(4)});
+    await gov.connect(op5).applyOperator(0x02, testPkX5, testRpcUrl5, testIntro5, {value: minSelfStakeAmt.add(5)});
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,         totalStakedAmt,  electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(1), 0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(2), minSelfStakeAmt.add(2), 0],
+      [op3.address, 0x02, testPkX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3), minSelfStakeAmt.add(3), 0],
+      [op4.address, 0x03, testPkX4, testRpcUrl4, testIntro4, minSelfStakeAmt.add(4), minSelfStakeAmt.add(4), 0],
+      [op5.address, 0x02, testPkX5, testRpcUrl5, testIntro5, minSelfStakeAmt.add(5), minSelfStakeAmt.add(5), 0],
+    ]);
+
+    await time.increase(MIN_STAKE_PERIOD + 1);
+    await gov.connect(op2).unstakeOperator(1, minSelfStakeAmt.add(2));
+    await gov.connect(op4).unstakeOperator(3, minSelfStakeAmt.add(4));
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,         totalStakedAmt,  electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(1), 0],
+      [zeroAddr,    0x00, zeroBytes32, zeroBytes32, zeroBytes32,                 0,                      0,  0],
+      [op3.address, 0x02, testPkX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3), minSelfStakeAmt.add(3), 0],
+      [zeroAddr,    0x00, zeroBytes32, zeroBytes32, zeroBytes32,                 0,                      0,  0],
+      [op5.address, 0x02, testPkX5, testRpcUrl5, testIntro5, minSelfStakeAmt.add(5), minSelfStakeAmt.add(5), 0],
+    ]);
+
+    await gov.connect(op2).applyOperator(0x03, testPkX2, testRpcUrl2, testIntro2, {value: minSelfStakeAmt.add(7)});
+    await gov.connect(op4).applyOperator(0x03, testPkX4, testRpcUrl4, testIntro4, {value: minSelfStakeAmt.add(6)});
+    expect(await getAllOperatorInfos(gov)).to.deep.equal([
+      // addr, pubkeyPrefix, pubkeyX, rpcUrl,      intro,      selfStakedAmt,         totalStakedAmt,  electedTime
+      [op1.address, 0x02, testPkX1, testRpcUrl1, testIntro1, minSelfStakeAmt.add(1), minSelfStakeAmt.add(1), 0],
+      [op4.address, 0x03, testPkX4, testRpcUrl4, testIntro4, minSelfStakeAmt.add(6), minSelfStakeAmt.add(6), 0],
+      [op3.address, 0x02, testPkX3, testRpcUrl3, testIntro3, minSelfStakeAmt.add(3), minSelfStakeAmt.add(3), 0],
+      [op2.address, 0x03, testPkX2, testRpcUrl2, testIntro2, minSelfStakeAmt.add(7), minSelfStakeAmt.add(7), 0],
+      [op5.address, 0x02, testPkX5, testRpcUrl5, testIntro5, minSelfStakeAmt.add(5), minSelfStakeAmt.add(5), 0],
+    ]);
   });
 
 });
@@ -143,12 +285,13 @@ async function getAllOperatorInfos(gov) {
   return ops;
 }
 async function getOperatorInfo(gov, idx) {
-  return await gov.operators(idx);
-  // let [addr, pubkeyPrefix, pubkeyX, rpcUrl, intro, totalStakedAmt, selfStakedAmt, electedTime] = await gov.operators(idx);
-  // // totalStakedAmt = ethers.utils.formatUnits(totalStakedAmt);
-  // // selfStakedAmt = ethers.utils.formatUnits(selfStakedAmt);
-  // electedTime = electedTime.toNumber();
-  // return [addr, pubkeyPrefix, pubkeyX, rpcUrl, intro, totalStakedAmt, selfStakedAmt, electedTime];
+  // return await gov.operators(idx);
+  let [addr, pubkeyPrefix, pubkeyX, rpcUrl, intro, totalStakedAmt, selfStakedAmt, electedTime] = await gov.operators(idx);
+  // totalStakedAmt = ethers.utils.formatUnits(totalStakedAmt);
+  // selfStakedAmt = ethers.utils.formatUnits(selfStakedAmt);
+  // pubkeyPrefix = pubkeyPrefix.toNumber();
+  electedTime = electedTime.toNumber();
+  return [addr, pubkeyPrefix, pubkeyX, rpcUrl, intro, selfStakedAmt, totalStakedAmt, electedTime];
 }
 
 async function getAllStakeInfos(gov) {
@@ -164,7 +307,8 @@ async function getAllStakeInfos(gov) {
   return infos;
 }
 async function getStakeInfo(gov, idx) {
-  return await gov.stakeInfos(idx);
-  // let [staker, operator, stakedTime, stakedAmt] = await gov.stakeInfos(idx);
-  // return [staker, operator, stakedTime, stakedAmt];
+  // return await gov.stakeInfos(idx);
+  let [staker, operator, stakedTime, stakedAmt] = await gov.stakeInfos(idx);
+  let stakedTimeGT0 = stakedTime > 0;
+  return [staker, operator, stakedAmt, stakedTimeGT0];
 }
